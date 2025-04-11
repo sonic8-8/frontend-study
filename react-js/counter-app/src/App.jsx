02b434ca2e40bb958d82ba6a58bcb34a187d7913
이승환 <seaung13@gmail.com>
2025-04-11 23:20:13 +0900

    feat: 라이프사이클, useEffect 예제 추가 및 학습 내용 주석으로 정리

--- react-js/counter-app/src/App.jsx
+++ react-js/counter-app/src/App.jsx
@@ -1,11 +1,38 @@
 import Controller from "./components/Controller";
 import Viewer from "./components/Viewer";
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
+import Even from "./components/Even";
 
 function App() {
 
   const [count, setCount] = useState(0);
+
+  const [input, setInput] = useState("");
+
+  const isMount = useRef(false);
+
+  // 1. 마운트
+  useEffect(() => {
+    console.log("mount");
+  }, []);
+
+  // 2. 업데이트
+  useEffect(() => {
+    if (!isMount.current) {
+      isMount.current = true;
+      return;
+    }
+    console.log("update");
+  });
+
+  // 3. 언마운트
+  // Even에 있습니다
+
+  // 의존성 배열 (dependency array -> deps )
+  useEffect(() => {
+    console.log(`count: ${count} / input: ${input}`);
+  }, [count, input])
 
   const onClickButton = (value) => {
     setCount(count + value);
@@ -15,7 +42,11 @@ function App() {
     <div className="App">
       <h1>Simple Counter</h1>
       <section>
+        <input onChange={(e) => {
+          setInput(e.target.value);
+        }} />
         <Viewer count={count} />
+        {count % 2 == 0 ? <Even /> : null}
       </section>
 
       <section>
