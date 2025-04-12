0f9cc66851ef2f71c99eefaedc06be2eb3723fac
이승환 <seaung13@gmail.com>
2025-04-13 02:39:25 +0900

    feat: todo-app에 useMemo, useCallback, React.memo를 적용해 렌더링 최적화

--- react-js/todo-app/src/App.jsx
+++ react-js/todo-app/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import Header from './components/Header'
 import Editor from './components/Editor'
 import List from './components/List'
-import { useState, useRef, useReducer } from 'react'
+import { useState, useRef, useReducer, useCallback } from 'react'
 import Exam from './components/Exam'
 
 const mockData = [
@@ -47,7 +47,7 @@ function App() {
   const [todos, dispatch] = useReducer(reducer, mockData);
   const idRef = useRef(3);
 
-  const onCreate = (content) => {
+  const onCreate = useCallback((content) => {
     dispatch({
       type: "CREATE",
       data: {
@@ -57,22 +57,21 @@ function App() {
         date: new Date().getTime(),
       }
     });
-  };
+  }, []);
 
-  const onUpdate = (targetId) => {
+  const onUpdate = useCallback((targetId) => {
     dispatch({
       type: "UPDATE",
       targetId: targetId,
     });
-  };
+  }, []);
 
-  // 삭제하려는 대상의 id와 일치하지 않는 것들만 배열로 만드는 것임
-  const onDelete = (targetId) => {
+  const onDelete = useCallback((targetId) => {
     dispatch({
       type: "DELETE",
       targetId: targetId,
     })
-  }
+  }, []);
 
   return (
     <div className="App">
--- react-js/todo-app/src/components/TodoItem.jsx
+++ react-js/todo-app/src/components/TodoItem.jsx
@@ -1,4 +1,5 @@
 import "./TodoItem.css";
+import { memo } from "react";
 
 const TodoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
     const onChangeCheckBox = () => {
@@ -19,4 +20,4 @@ const TodoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
     )
 }
 
-export default TodoItem;
+export default memo(TodoItem);
