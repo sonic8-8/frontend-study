6d9f738ece127a8b7ff30b35f61249ec0cd278fb
이승환 <seaung13@gmail.com>
2025-04-16 05:12:04 +0900

    feat: emotion-diary-app 배포 준비 (title 동적 변경, favicon, OG 태그)

--- react-js/emotion-diary-app/src/pages/Diary.jsx
+++ react-js/emotion-diary-app/src/pages/Diary.jsx
@@ -6,11 +6,13 @@ import Button from "../components/Button";
 import Viewer from "../components/Viewer";
 import useDiary from "../hooks/useDiary";
 import { getStringedDate } from "../util/get-stringed-date";
+import usePageTitle from "../hooks/usePageTitle";
 
 const Diary = () => {
     const params = useParams();
     const nav = useNavigate();
     const currentDiaryItem = useDiary(params.id);
+    usePageTitle(`${params.id}번 일기`);
 
     if (!currentDiaryItem) {
         return <div>데이터 로딩중...</div>
